43bf8e3d5597ce3d04dc8eb2d9eb9cdb4b79b3ab
Lavanya <lavanyace929@gmail.com>
2021-02-22 15:19:53 +0530

    Added:The Choice Of Players Was Taken

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,7 +1,9 @@
+import java.util.*;
 public class TicTacToe {
 
     //Static ArrayVariable
     public static char[] board = new char[10];
+	 public static char personChoice, compChoice;
 
     //CreateBoard() method to initialise a char array of size 10 
     public static void createBoard() {
@@ -10,7 +12,26 @@ public class TicTacToe {
         }
     }
 
+	//playerChoice() method to choose Person's and Computer's choice 
+	public static void playerChoice() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Choose letter 'X' or 'O': ");
+		personChoice = sc.next().charAt(0);
+		if ( personChoice == 'X' || personChoice == 'x') {
+			personChoice = 'X';
+			compChoice = 'O';
+		}
+		else {
+			personChoice = 'O';
+			compChoice = 'X';
+		}
+		System.out.println("User: "+personChoice+"\nComputer: "+compChoice);
+	}
+
+
+
     public static void main(String[] args) {
         createBoard();
+		  playerChoice();
     }
 }
