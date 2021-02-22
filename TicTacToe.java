6460d2e3323c624ab0cba972044d3746123b37c9
Lavanya <lavanyace929@gmail.com>
2021-02-22 17:47:33 +0530

    Added:Displayed It Is A Win Or Not

--- TicTacToe.java
+++ TicTacToe.java
@@ -3,6 +3,7 @@ public class TicTacToe {
 	//Static ArrayVariable
 	public static char[] board = new char[10];
 	public static char personChoice, compChoice;
+	public static String result;
 
 	//CreateBoard() method to initialise a char array of size 10 
 	public static void createBoard() {
@@ -47,24 +48,27 @@ public class TicTacToe {
 					board[number] = personChoice;
 					break;
 				}
-				else
-					System.out.println("Position Occupies Already");
+				else {
+						System.out.println("Position Occupies Already");
+						continue;
+				}
 			}
-			else
-				System.out.println("InValid Input Number");
+			else {
+					System.out.println("InValid Input Number");
+					continue;
+			}
 		}
 	}
 	public static void compPlay() {
-		int random = (int)(Math.floor(Math.random() * 10) % 9)+1;
-		if (random > 0 && random < 10) {
+		while (true) {
+			int random = (int)(Math.floor(Math.random() * 10) % 9)+1;
 			if (board[random] == ' ') {
 				board[random] = compChoice;
+				break;
 			}
 			else
-				System.out.println("Position Allready Occupied");
+					continue;
 		}
-		else
-			System.out.println("InValid Input Number");
 	}
 
 	public static boolean tossToKnow(){
@@ -78,19 +82,98 @@ public class TicTacToe {
 			return false;
 		}
 	}
+	/*WinnerCheck() method is to check either X or O has won the game or the game is draw */
+	static String winnerCheck() {
+		for (int j = 0; j < 8; j++) {
+			switch (j) {
+				case 0:
+					result = new StringBuilder().append(board[1]).append(board[2]).append(board[3]).toString();
+					break;
+				case 1:
+					result = new StringBuilder().append(board[4]).append(board[5]).append(board[6]).toString();
+					break;
+				case 2:
+					result = new StringBuilder().append(board[7]).append(board[8]).append(board[9]).toString();
+					break;
+				case 3:
+					result = new StringBuilder().append(board[1]).append(board[4]).append(board[7]).toString();
+					break;
+				case 4:
+					result = new StringBuilder().append(board[2]).append(board[5]).append(board[8]).toString();
+					break;
+				case 5:
+					result = new StringBuilder().append(board[3]).append(board[6]).append(board[9]).toString();
+					break;
+				case 6:
+					result = new StringBuilder().append(board[1]).append(board[5]).append(board[9]).toString();
+					break;
+				case 7:
+					result = new StringBuilder().append(board[3]).append(board[5]).append(board[7]).toString();
+					break;
+			}
+			//If Winner Is X
+			if (result.equals("XXX")) 
+				return "X";
+
+			//If Winner Is O
+			else if (result.equals("OOO")) 
+				return "O";
+            
+		}
+		// In case of No-Winner 
+		for (int i = 0; i < board.length; i++) {
+			if (board[i] == ' ') {
+				return null;
+			}
+		}
+        return "draw";
+    }
+    // displayWinner method Prints The Winner Or Draw
+    public static void displayWinner (String outPut) {
+        if (outPut == "draw") {
+            System.out.println("It Is Draw");
+            System.exit(0);
+        }
+        else if(outPut == "X" || outPut == "O"){
+            System.out.println("The winner is : " + outPut);
+            System.exit(0);
+        }
+    }
+
+
 
 	public static void main(String[] args) {
+		String winner = null;
 		createBoard();
 		playerChoice();
 		displayBoard();
 		boolean get = tossToKnow();
-		while (true) {
-			if (get)
-				playerPlay();
-			else
-				compPlay();
-			displayBoard();
-			break;
-		}
+        if (get){
+            while (winner == null) {
+                playerPlay();
+                displayBoard();
+                winner = winnerCheck();
+                displayWinner(winner);
+                System.out.println();
+                compPlay();
+                displayBoard();
+                winner = winnerCheck();
+                displayWinner(winner);
+            }
+        }
+        else {
+            while (winner == null) {
+                compPlay();
+                displayBoard();
+                winner = winnerCheck();
+                displayWinner(winner);
+                System.out.println();
+                playerPlay();
+                displayBoard();
+                winner = winnerCheck();
+                displayWinner(winner);
+            }
+        }		
 	}
+
 }
